a285b40fe1d731bcf5570b135cdac357ed0db53d
unknown <emilio.rodriguez@correo.ucu.edu.uy>
2025-10-02 23:18:41 -0300

    Chatbot creado

---
+++ src/main/main.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import this
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from dotenv import load_dotenv
+
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    
+if __name__ == '__main__':
+    load_dotenv() 
+    token = os.getenv("token")
+    application = ApplicationBuilder().token(token).build()
+        
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
